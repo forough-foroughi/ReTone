e565640dd159544f1b7bdc11cc3da10ce8fc2ebd
forough-foroughi <foroughi.forough@gmail.com>
2025-11-05 20:43:02 +0100

    Add terraform to deploy on kubernetes by helm

---
+++ infra/main.tf
@@ -0,0 +1,63 @@
+resource "kubernetes_namespace" "ns" {
+  metadata { name = var.namespace }
+}
+
+resource "helm_release" "retone" {
+  name = "retone"
+  namespace = kubernetes_namespace.ns.metadata[0].name
+  chart = var.chart_path
+
+  set = [ 
+    {
+        name = "image.repository"
+        value = var.image_repo
+    }, 
+    {
+        name = "image.tag"
+        value = var.image_tag
+    },
+    {
+        name = "service.port"       
+        value = var.service_port
+    },
+    {
+        name = "service.targetPort"
+        value = var.service_targetPort
+    },
+    {
+        name = "secretEnv.OPENROUTER_API_KEY.secretName"
+        value = var.secret_name
+    },
+    {
+        name = "secretEnv.OPENROUTER_API_KEY.key"
+        value = "OPENROUTER_API_KEY"
+    },
+    {
+        name = "ingress.enabled"
+        value = "true"
+    },
+    {
+        name = "ingress.className"
+        value = var.ingress_classname
+    },
+    {
+        name = "ingress.hosts[0].host"
+        value = var.host
+    },
+    {
+        name = "ingress.hosts[0].path"
+        value = "/"
+    },
+    {
+        name = "ingress.hosts[0].pathType"
+        value = "prefix"
+    }
+  ]
+}
+
+
+
+
+
+ 
+
---
+++ infra/variables.tf
@@ -0,0 +1,55 @@
+variable "kubeconfig_path" {
+    type = string
+    default = "~/.kube/config"
+}
+
+variable "kube_context" {
+    type = string
+    default = null                      # set if you have many contexts
+}
+
+variable "namespace" {
+    type = string
+    default = "retone"
+}
+
+variable "chart_path" { 
+    type = string  
+    default = "../charts/retone" 
+}
+
+variable "image_repo" { 
+    type = string  
+    default = "242242/retone" 
+}
+
+variable "image_tag" { 
+    type = string  
+    default = "v0.1" 
+}
+
+variable "secret_name" { 
+    type = string  
+    default = "retone-secrets" 
+}
+
+variable "host" {                       #ingress config
+    type = string  
+    default = "retone.forough-foroughi.com" 
+}
+
+variable "ingress_classname" {
+    type = string
+    default = "traefik"
+  
+}
+
+variable "service_port" { 
+    type = string  
+    default = "80" 
+}
+
+variable "service_targetPort" { 
+    type = string  
+    default = "5000" 
+}
